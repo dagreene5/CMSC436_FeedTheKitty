53a3ca85c9047fa57245dd057d60c52c0f77d2c3
Sang Lee <Lee@Sangs-MacBook-Pro.local>
2015-11-17 09:57:38 -0500

    action bar search and home view

--- FeedTheKitty/app/src/main/java/com/feedthekitty/cmsc436/feedthekitty/HomeActivity.java
+++ FeedTheKitty/app/src/main/java/com/feedthekitty/cmsc436/feedthekitty/HomeActivity.java
@@ -1,21 +1,48 @@
 package com.feedthekitty.cmsc436.feedthekitty;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
+/**
+ *  Homescreen of the app. User will be taken to this view when they're logged in via FB.
+ *
+ *  @Author     Sang Lee
+ *  @Version    1.0
+ */
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button browse_btn = (Button) findViewById(R.id.browse_btn);
+        Button create_btn = (Button) findViewById(R.id.create_btn);
+
+        //Listener on browse button
+        browse_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Intent browse = new Intent(this, BrowseActivity.class);
+                //startActivity(browse);
+            }
+        });
+
+        //Listener on create button
+        create_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Intent create = new Intent(this, CreateActivity.class);
+                //startActivity(create);
+            }
+        });
     }
 
     @Override
@@ -33,20 +60,5 @@ public class MainActivity extends AppCompatActivity {
                 searchManager.getSearchableInfo(getComponentName()));
 
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
